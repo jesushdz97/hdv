4d86c9cec13480140aef8d93b9225f18a44a0484
Jesus Hernandez <jehersi988@gmail.com>
2023-04-10 21:57:32 -0500

    fix vite config: base

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hdv/',
   resolve: {
-    base: '/hdv/',
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
